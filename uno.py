5802b2a36396f8f9f16e43f3a6b43715d2008692
Dennys <dennys028ss@gmail.com>
2025-07-21 19:53:58 -0600

    ejercicio2

--- uno.py
+++ uno.py
@@ -31,3 +31,26 @@ if canvote:
     print(f"Bienvenido, {name}. Su centro de votacion esta en {departament} ")
 else:
     print("No puedes votar.")
+
+
+
+#SALARIO
+
+salary = float(input("Ingrese su salario: "))
+dependents = int(input("Ingrese el numero de dependientes: "))
+
+if salary < 40000 and dependents > 2:
+    print("No paga impuestos")
+else:
+    if 0 <= salary <= 30000:
+        print("Paga el 5%")
+    elif 30001 <= salary <= 60000:
+        print("Paga el 10%")
+    elif 60001 <= salary <= 100000:
+        print("Paga el 15%")
+    elif salary > 100000:
+        print("Paga el 20%")
+
+    print("La deduccion por dependientes es de: ", dependents * 1000)
+
+
